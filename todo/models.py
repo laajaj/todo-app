b37ff3017ef7f9be5fbc6ab0fb6729999d2a8e9d
Younes Laajaj <younes.laajaj@unyc.io>
2024-12-15 20:15:34 +0100

    add models for relatioips

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,23 @@
 
 from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Priority(models.Model):
+    level = models.CharField(max_length=50)  # Par exemple, "Haute", "Moyenne", "Basse"
+    
+    def __str__(self):
+        return self.level
+
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
@@ -7,6 +25,9 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     due_date = models.DateTimeField(null=True, blank=True)
     is_completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # Relation un-à-plusieurs
+    projects = models.ManyToManyField(Project, related_name='tasks', null=True, blank=True)  # Relation plusieurs-à-plusieurs
+    priority = models.OneToOneField(Priority, on_delete=models.SET_NULL, null=True, blank=True)  # Relation un-à-un
 
     def __str__(self):
         return self.title
