cc432b22e390b71510a0fdfe254ec0b980f67b83
Younes Laajaj <younes.laajaj@unyc.io>
2025-02-23 20:40:17 +0100

    try de deploy on vercel

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2l5kx$7!=tpp=(nx5chn7obxfkoukg1i_s^8_cjh-au4bq2v$z"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
